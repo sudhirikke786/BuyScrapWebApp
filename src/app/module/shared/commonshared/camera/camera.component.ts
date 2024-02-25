ed187d1077056316ae448cb5a7be8021ade91088
Kisan <kisanpwr1@gmail.com>
2024-02-25 22:31:38 +0530

    files added

--- src/app/module/shared/commonshared/camera/camera.component.ts
+++ src/app/module/shared/commonshared/camera/camera.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, AfterViewInit, Output, OnDestroy } from '@angu
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { Observable, Subject } from 'rxjs';
 
+declare var Tesseract:any 
+
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
@@ -82,10 +84,47 @@ export class CameraComponent implements AfterViewInit ,OnDestroy {
     }
   }
 
-  handleImage(webcamImage: WebcamImage) {
+
+   async convertIntoOCR(imgUrl:any) {
+    const index =  imgUrl.indexOf('base64,') + 7; // Find the position of ','
+    const base64Data = imgUrl.substring(index); // Extract base64 data
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+  
+    // Create a Blob from the binary data
+    const blob = new Blob([byteArray], { type: 'image/jpeg' }); // Change the type if necessary
+    
+    // Generate a local URL for the Blob
+    const url = URL.createObjectURL(blob);
+
+    try {
+      const result =  await Tesseract.recognize(url);
+      console.log(result); // Log the result object
+      const dta = result?.text;
+      const textArray = dta.split('\n'); // Split by line breaks, adjust the delimiter based on your text format
+       return textArray
+    
+    } catch (error) {
+      console.error('Error during OCR:', error);
+    }
+  }
+
+
+
+  async handleImage(webcamImage: WebcamImage) {
     this.webcamImage = webcamImage;
     this.imageUrl = webcamImage.imageAsDataUrl;
    
+
+
+    const _textData = await this.convertIntoOCR(this.imageUrl);
+
+    console.log(_textData);
+
     this.getPicture.emit(this.imageUrl);
 
     
@@ -99,17 +138,63 @@ export class CameraComponent implements AfterViewInit ,OnDestroy {
     return this.nextWebcam.asObservable();
   }
   
+
+  // async  recognizeText(image: string): Promise<string> {
+  //   const result = await Tesseract.recognize(image);
+  //   return result.text;
+  // }
+
+
+
+  // async recognizeText() {
+  //   const image = 'assets/img/my-image.jpg';
+  //   const text = await recognizeText(image);
+  //   console.log(text);
+  // }
+
   
-  onFileChanged(event: any) {
+   onFileChanged(event: any) {
     this.imageUrl = null;
     const file = event.target.files[0];
     console.log('file  :: ' + JSON.stringify(file));
     let reader = new FileReader();
     if (event.target.files && event.target.files[0]) {
       reader.readAsDataURL(file);
-      reader.onload = () => {
+      reader.onload = async () => {
         this.imageUrl = reader.result;
+
+        const _textData = await this.convertIntoOCR(this.imageUrl);
+
+        console.log(_textData);
+
         this.getPicture.emit(this.imageUrl);
+
+        // const index = this.imageUrl.indexOf('base64,') + 7; // Find the position of ','
+        // const base64Data = this.imageUrl.substring(index); // Extract base64 data
+        // const byteCharacters = atob(base64Data);
+        // const byteNumbers = new Array(byteCharacters.length);
+        // for (let i = 0; i < byteCharacters.length; i++) {
+        //   byteNumbers[i] = byteCharacters.charCodeAt(i);
+        // }
+        // const byteArray = new Uint8Array(byteNumbers);
+      
+        // // Create a Blob from the binary data
+        // const blob = new Blob([byteArray], { type: 'image/jpeg' }); // Change the type if necessary
+        
+        // // Generate a local URL for the Blob
+        // const url = URL.createObjectURL(blob);
+
+        // try {
+        //   const result = await Tesseract.recognize(url);
+        //   console.log(result); // Log the result object
+        //   const dta = result?.text;
+        //   const textArray = dta.split('\n'); // Split by line breaks, adjust the delimiter based on your text format
+        //   alert(textArray)
+        //   console.log(dta); // Log the recognized text
+       
+        // } catch (error) {
+        //   console.error('Error during OCR:', error);
+        // }
       }       
     }
     // Clear the input
