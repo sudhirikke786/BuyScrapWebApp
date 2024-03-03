2d0a09657c52e66c90bab66c4d2d4da232bc309b
Kisan <kisanpwr1@gmail.com>
2024-03-03 14:57:38 +0530

    Files added scussess page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
     component: SignUpComponent
   },
   {
-    path:'success',
+    path:'payment-page',
     component:SuccessPageComponent
   },
   { 
--- src/app/success-page/success-page.component.ts
+++ src/app/success-page/success-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-page.component.css']
 })
 export class SuccessPageComponent {
+
+    pageType:any
+
+  constructor(private route : Router, private activeRoute: ActivatedRoute){
+
+    this.activeRoute.queryParams.subscribe((res) => {
+       this.pageType = res;
+    })
+
+  }
 
 }
