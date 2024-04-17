a5d2d420ed1a1360d683b7155fe2ad94af152165
Kisan <kisanpwr1@gmail.com>
2024-04-17 08:18:48 +0530

    Files added

--- src/app/module/shared/commonshared/plan-upgrade/plan-upgrade.component.ts
+++ src/app/module/shared/commonshared/plan-upgrade/plan-upgrade.component.ts
@@ -7,6 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 export class PlanUpgradeComponent {
 
+  orgName:any;
+
   @Input() message =  'Upgrade To plus Version Required ';
+
+
+  constructor(){
+    this.orgName = localStorage.getItem('orgName');
+  }
 
 }
