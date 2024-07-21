093452385227d6eea7961e4ebadab7b3f42ee5a2
Kisan <kisanpwr1@gmail.com>
2024-07-21 20:46:40 +0530

    Files added report

--- src/app/module/reports/requisition-report/requisition-report.component.ts
+++ src/app/module/reports/requisition-report/requisition-report.component.ts
@@ -1,28 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from 'src/app/core/services/common.service';
+
+interface DataRow {
+  dateAcquired: string;
+  time: string;
+  ticketNo: number;
+  name: string;
+  address: string;
+  idNumber: string;
+  commodity: string;
+  quantity: number;
+}
+
+
 
 @Component({
   selector: 'app-requisition-report',
   templateUrl: './requisition-report.component.html',
   styleUrls: ['./requisition-report.component.css']
 })
-export class RequisitionReportComponent {
+export class RequisitionReportComponent implements OnInit {
 
-  data = [
-    {
-      DateAcquired: "2024-05-23",
-      Time: "08:41",
-      TicketNo: "1234",
-      Name: "Leonard Simon",
-      Address: ["65 Kabeljou Str.", "Kuisebmond"],
-      IDNumber: "820112546",
-      Commodities: [
-        { Commodity: "Copper", QuantityKg: 0.88 },
-        { Commodity: "Dirty Aluminium", QuantityKg: 5 },
-        { Commodity: "Brass", QuantityKg: 0.18 },
-        { Commodity: "Light Steel", QuantityKg: 3 }
-      ]
-    }
+
+    
+  orgName: any;
+  locId: any;
+  subScriptionType: any;
+  showPlan = false;
+  constructor(private route: ActivatedRoute,
+   
+ 
+    private commonService: CommonService) { }
+
+  ngOnInit() {
+    this.orgName = localStorage.getItem('orgName');
   
+
+    this.locId = this.commonService.getProbablyNumberFromLocalStorage('locId');
+  }
+
+  
+
+  data: DataRow[] = [
+    { dateAcquired: '23.05.2024', time: '08h41', ticketNo: 1234, name: 'Leonard Simon', address: '65 Kabeljou Str.\nKuisebmond', idNumber: '820112546', commodity: 'Copper', quantity: 0.88 },
+    { dateAcquired: '23.05.2024', time: '08h41', ticketNo: 1234, name: 'Leonard Simon', address: '65 Kabeljou Str.\nKuisebmond', idNumber: '820112546', commodity: 'Dirty Aluminium', quantity: 5 },
+    { dateAcquired: '23.05.2024', time: '08h41', ticketNo: 1234, name: 'Leonard Simon', address: '65 Kabeljou Str.\nKuisebmond', idNumber: '820112546', commodity: 'Brass', quantity: 0.18 },
+    { dateAcquired: '23.05.2024', time: '08h41', ticketNo: 1234, name: 'Leonard Simon', address: '65 Kabeljou Str.\nKuisebmond', idNumber: '820112546', commodity: 'Light Steel', quantity: 3 },
+   
   ];
+
+  getRowSpan(data: DataRow[], key: keyof DataRow, value: any): number {
+    return data.filter(item => item[key] === value).length;
+  }
+
+  isFirstOccurrence(data: DataRow[], key: keyof DataRow, index: number): boolean {
+    return data.findIndex(item => item[key] === data[index][key]) === index;
+  }
 
 }
