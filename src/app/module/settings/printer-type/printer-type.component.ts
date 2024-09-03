f050111135bbc5feaa3b28b0b6fdbd8f3dbc97c4
Kisan <kisanpwr1@gmail.com>
2024-09-03 22:21:56 +0530

    Files added for printer type

--- src/app/module/settings/printer-type/printer-type.component.ts
+++ src/app/module/settings/printer-type/printer-type.component.ts
@@ -13,6 +13,11 @@ export class PrinterTypeComponent implements OnInit {
 @Output() close = new EventEmitter<boolean>();
   defaultReceiptPrinter: string = 'Normal';
 
+
+  
+  passprntUri: string = '';
+  passprntPDfURL: string = '';
+
   
   constructor( private messageService: MessageService) { }
 
@@ -35,6 +40,31 @@ export class PrinterTypeComponent implements OnInit {
 
   closePopup(){
     this.close.emit()
+  }
+
+
+  createURI(): void {
+    const pdfstr ='JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDU1ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDAgMCBUZAogICAgKEhlbGxvIFdvcmxkKSBUagogIEVUCmVuZHN0cmVhbQplbmRvYmoKCnhyZWYKMCA1CjAwMDAwMDAwMDAgNjU1MzUgZiAKMDAwMDAwMDAxOCAwMDAwMCBuIAowMDAwMDAwMDc3IDAwMDAwIG4gCjAwMDAwMDAxNzggMDAwMDAgbiAKMDAwMDAwMDQ1NyAwMDAwMCBuIAp0cmFpbGVyCiAgPDwgIC9Sb290IDEgMCBSCiAgICAgIC9TaXplIDUKICA+PgpzdGFydHhyZWYKNTY1CiUlRU9GCg=='
+
+    this.passprntPDfURL = "starpassprnt://v1/print/nopreview?";
+
+    this.passprntPDfURL += "back=" + encodeURIComponent(window.location.href);
+
+    this.passprntPDfURL += "&pdf=" + encodeURIComponent(pdfstr);
+
+    // This will be used to update the href of the anchor tag in the template
+  }
+
+
+  createHtMl(): void {
+
+    this.passprntUri = "starpassprnt://v1/print/nopreview?";
+
+    this.passprntUri += "back=" + encodeURIComponent(window.location.href);
+
+    this.passprntUri += "&html=" + encodeURIComponent('<html><head></head><body>Hellow Word</body></html>');
+
+    // This will be used to update the href of the anchor tag in the template
   }
 
 }
